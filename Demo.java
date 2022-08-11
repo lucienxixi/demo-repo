64348e7eb0f8e2eff9e9206d7e373b896ef589c1
lucienxixi <liuanxi0528@outlook.com>
2022-08-11 00:41:31 -0400

    added new method

--- Demo.java
+++ Demo.java
@@ -2,4 +2,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
+    public void method1() {
+        System.out.println("method1");
+    }
 }
